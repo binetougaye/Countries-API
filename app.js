ca4cec02155f21c111b7b4fdb33dd89737ebaabf
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2023-12-29 22:49:29 +0100

    affiche détail

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let darkOrLight = document.querySelector(".sun");
 let faMoon = document.querySelector(".fa-moon-o");
 let faSun = document.querySelector(".fa-sun-o");
 let pays = document.querySelector(".pays");
+// console.log(pays);
 let span = document.querySelector("span");
 
 let body = document.querySelector("body");
@@ -19,7 +20,6 @@ function modeDarkLight() {
 darkOrLight.addEventListener("click", modeDarkLight);
 
 // // GESTION DE L'API
-// // SEARCH BY COUNTRY NAME
 // // Variables
 let countryName = document.querySelector(".title");
 let population = document.querySelector(".card-text");
@@ -27,6 +27,7 @@ let region = document.querySelector(".region");
 let capital = document.querySelector(".capital");
 let picture = document.querySelector(".img");
 let cards = document.querySelector(".cards");
+let bodyS = document.querySelector("body");
 
 console.log(pays);
 // pays.style.height = "300px";
@@ -37,57 +38,84 @@ async function allCountry() {
   const response = await fetch(apiAll);
   let data = await response.json();
   console.log(data);
-  for (i = 0; i < data.length; i++) {
-    pays.innerHTML += `<div class="shadow rounded" style="width: 18rem;height: 295px;>
-    <div class="card-body p-2">
-      <img src="${data[i].flags.png}"   style="height: 144px;object-fit: cover;">
-      <h4 class="name ms-2 mt-2">${data[i].name.common}</h4>
-      <h6 class="population mt-2  ms-2">
-        Population: ${data[i].population}</h6>
-      <h6 class="region mt-2  ms-2">Region: ${data[i].continents}</h6>
-      <h6 class="capital mt-2  ms-2">Capital: ${data[i].capital}</h6>
-    </div>
-</div>`;
-  }
 }
 // cards.innerHTML += pays;
 allCountry();
 // // FILTRER LES PAYS PAR CONTINENT
+//
+async function getCountryByCountry(nom) {
+  let data = await fetch("https://restcountries.com/v3.1/name/" + nom);
+  console.log(data);
+  let response = await data.json();
+  console.log(response);
+  return response;
+}
 
-// const apiByContinent = "https://restcountries.com/v3.1/region/";
-// let selectInput = document.getElementById("select");
-// async function getCountryByContinent(country) {
-//   const response = await fetch(apiByContinent + country);
-//   let data = await response.json();
+const apiByContinent = "https://restcountries.com/v3.1/region/";
+let selectInput = document.getElementById("select");
+async function getCountryByContinent(region) {
+  const response = await fetch(apiByContinent + region);
+  let data = await response.json();
+  return data;
+}
+let container = document.getElementById("countries");
 
-//   return data;
-// }
+selectInput.addEventListener("change", async function () {
+  const data = await getCountryByContinent(selectInput.value);
+  pays.innerHTML = "";
+  for (i = 0; i < data.length; i++) {
+    pays.innerHTML += `
+    <div class="shadow rounded ashs" style="width: 18rem;height: 300px;>
+     <div class="card-body p-2">
+          <img src="${data[i].flags.png}" style="height: 144px;object-fit: cover;">
+         <h4 class="name ms-2 mt-2">${data[i].name.common}</h4>
+          <h6 class="population mt-2 ms-2">
+            Population: ${data[i].population}</h6>
+    <h6 class="region mt-2  ms-2">Region: ${data[i].continents}</h6>
+    <h6 class="capital mt-2  ms-2">Capital: ${data[i].capital}</h6>
+    </div>
+    </div>
+    `;
+  }
+  // DETAIL DES PAYS
+  let test = document.querySelectorAll(".ashs");
+  let title = document.querySelectorAll(".name");
 
-// selectInput.addEventListener("change", async function () {
-//   const data = await getCountryByContinent(selectInput.value);
-//   pays.innerHTML = "";
-//   console.log(data);
-//   for (i = 0; i < data.length; i++) {
-//     pays.innerHTML += ` <div class="card shadow" style="width: 18rem; heigth:300px">
-//       <img src="${data[i].flags.png}">
-//       <div class="card-body">
-//           <h4 class="name">${data[i].name.common}</h4>
-//           <h6 class="population">
-//            Population: ${data[i].population}</h6
-//           <h6 class="region">Region: ${data[i].continents}</h6>
-//           <h6 class="capital">Capital: ${data[i].capital}</h6>
-//       </div>
-//   </div>`;
-//   }
-// });
-
-// // ASIE
-
-// // const apiAsie = "https://restcountries.com/v3.1/region/asia";
-
-// // OCEANIE
-
-// // const apiOceanie = "https://restcountries.com/v3.1/region/oceania";
-
-// // AMERICA
-// // const apiAmerica = "https://restcountries.com/v3.1/region/america";
+  for (i = 0; i < test.length; i++) {
+    let name = title[i];
+    test[i].addEventListener("click", async function (e) {
+      const data = await getCountryByCountry(name.innerText);
+      console.log(data);
+      console.log(name.innerText);
+      console.log(name);
+      for (i = 0; i < data.length; i++) {
+        pays.innerHTML = `
+        <div class="row d-flex  justify-content-center align-items-center  ashs" >
+        <div class = "col-6">
+        <img src="${data[i].flags.png}">
+        </div>
+        <div class= "col-6 d-flex justify-content-center align-items-center">
+     <div class="card-body p-2">
+      <h4 class="name ms-2 mt-2">Native Name: ${data[i].name.common}</h4>
+      <h6 class="population mt-2 ms-2">Population: ${data[i].population}</h6>      
+      <h6 class="region mt-2  ms-2">Region: ${data[i].continents}</h6>
+      <h6 class="region mt-2  ms-2">Subregion: ${data[i].subregion}</h6>
+      <h6 class="capital mt-2  ms-2">Capital: ${data[i].capital}</h6>
+      </div>
+      <div>
+      <div>
+        Border Countries:
+        
+   
+      </div>
+      <h6 class=" mt-2  ms-2">:Top Level Domain${data[i].tld[0]}</h6>
+      <h6 class="population mt-2 ms-2"> Currencies: ${data[i].population}</h6>
+      <h6 class=" mt-2 ms-2"> Langages : ${data[i].region}</h6>
+    </div>
+   </div>
+    </div>
+        `;
+      }
+    });
+  }
+});
