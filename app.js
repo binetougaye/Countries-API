c9176111c16720e165521aefa63d5c66cc82cd38
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2024-01-03 00:05:50 +0100

    reload la page

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ let darkOrLight = document.querySelector(".sun");
 let faMoon = document.querySelector(".fa-moon-o");
 let faSun = document.querySelector(".fa-sun-o");
 let pays = document.querySelector(".pays");
-// console.log(pays);
+let searchInput = document.getElementById("searchInput");
+console.log(searchInput);
+let select = document.getElementById("select");
+console.log(select);
 let span = document.querySelector("span");
 
 let body = document.querySelector("body");
@@ -38,8 +41,76 @@ async function allCountry() {
   const response = await fetch(apiAll);
   let data = await response.json();
   console.log(data);
+  for (i = 0; i < data.length; i++) {
+    pays.innerHTML += `
+    <div class="shadow rounded ashs" style="width: 18rem;height: 300px;>
+     <div class="card-body p-2">
+          <img src="${data[i].flags.png}" style="height: 144px;object-fit: cover;">
+         <h4 class="name ms-2 mt-2">${data[i].name.common}</h4>
+          <h6 class="population mt-2 ms-2">
+            Population: ${data[i].population}</h6>
+    <h6 class="region mt-2  ms-2">Region: ${data[i].continents}</h6>
+    <h6 class="capital mt-2  ms-2">Capital: ${data[i].capital}</h6>
+    </div>
+    </div>
+    `;
+  }
+
+  let test = document.querySelectorAll(".ashs");
+  // console.log(test);
+  let title = document.querySelectorAll(".name");
+  console.log(title);
+  for (i = 0; i < test.length; i++) {
+    let name = title[i];
+    test[i].addEventListener("click", function () {
+      getCountryByCountry(name.innerText);
+
+      for (i = 0; i < test.length; i++) {
+        let name = title[i];
+        test[i].addEventListener("click", async function () {
+          const data = await getCountryByCountry(name.innerText);
+          for (i = 0; i < data.length; i++) {
+            searchInput.style.display = "none";
+            select.style.display = "none";
+            pays.innerHTML = `
+            <button class="border-0 p-1 rounded shadow button" style="width: 100px;" onclick="reloade()">
+            <i class="fa fa-arrow-left fs-6" aria-hidden="true"></i>
+            Back
+            </button>
+            <div class="row d-flex  justify-content-center align-items-center  ashs" >
+            <div class = "col-6">
+            <img src="${data[i].flags.png}">
+            </div>
+            <div class= "col-6 d-flex justify-content-center align-items-center">
+         <div class="card-body p-2">
+          <h4 class="name ms-2 mt-2">Native Name: ${data[i].name.common}</h4>
+          <h6 class="population mt-2 ms-2">Population: ${data[i].population}</h6>      
+          <h6 class="region mt-2  ms-2">Region: ${data[i].continents}</h6>
+          <h6 class="region mt-2  ms-2">Subregion: ${data[i].subregion}</h6>
+          <h6 class="capital mt-2  ms-2">Capital: ${data[i].capital}</h6>
+          </div>
+          <div>
+          <div>
+            Border Countries:
+            
+       
+          </div>
+          <h6 class=" mt-2  ms-2">:Top Level Domain${data[i].tld[0]}</h6>
+          <h6 class="population mt-2 ms-2"> Currencies: ${data[i].population}</h6>
+          <h6 class=" mt-2 ms-2"> Langages : ${data[i].region}</h6>
+        </div>
+       </div>
+        </div>
+            `;
+          }
+          let button = document.querySelector(".button");
+          console.log(button);
+        });
+      }
+    });
+  }
 }
-// cards.innerHTML += pays;
+
 allCountry();
 // // FILTRER LES PAYS PAR CONTINENT
 //
@@ -77,18 +148,17 @@ selectInput.addEventListener("change", async function () {
     </div>
     `;
   }
-  // DETAIL DES PAYS
   let test = document.querySelectorAll(".ashs");
+  // console.log(test);
   let title = document.querySelectorAll(".name");
-
+  console.log(title);
   for (i = 0; i < test.length; i++) {
     let name = title[i];
-    test[i].addEventListener("click", async function (e) {
+    test[i].addEventListener("click", async function () {
       const data = await getCountryByCountry(name.innerText);
-      console.log(data);
-      console.log(name.innerText);
-      console.log(name);
       for (i = 0; i < data.length; i++) {
+        searchInput.style.display = "none";
+        select.style.display = "none";
         pays.innerHTML = `
         <div class="row d-flex  justify-content-center align-items-center  ashs" >
         <div class = "col-6">
@@ -105,8 +175,6 @@ selectInput.addEventListener("change", async function () {
       <div>
       <div>
         Border Countries:
-        
-   
       </div>
       <h6 class=" mt-2  ms-2">:Top Level Domain${data[i].tld[0]}</h6>
       <h6 class="population mt-2 ms-2"> Currencies: ${data[i].population}</h6>
@@ -118,4 +186,9 @@ selectInput.addEventListener("change", async function () {
       }
     });
   }
+  // DETAIL DES PAYS
 });
+// Retourner à la page lors du clic
+function reloade() {
+  location.reload();
+}
